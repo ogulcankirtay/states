d04925895a07805c7f2b7bbb7e7545133a625a04
Ogulcan <ogulcankirtay8@gmail.com>
2023-03-05 01:24:31 +0300

    first_state_created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [name,setName]=useState("Oğulcan");
+  const [age,setAge]=useState(23);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Merhaba {name}</h1>
+      <h1>{age}</h1>
+      <button onClick={()=>{setName("Kırtay")}}>Change Name</button>
+      <button onClick={()=>setAge(age+1)}>Change Age</button>
     </div>
   );
 }
