c8859b77bea202fa63712f712ba7849910fb03ac
Ogulcan <ogulcankirtay8@gmail.com>
2023-03-05 21:29:53 +0300

    array_states

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import { useState } from "react";
 function App() {
   const [name,setName]=useState("Oğulcan");
   const [age,setAge]=useState(23);
+  const [firends,setFirends]=useState(["Serkan","Çağlar"])
   return (
     <div className="App">
       <h1>Merhaba {name}</h1>
       <h1>{age}</h1>
       <button onClick={()=>{setName("Kırtay")}}>Change Name</button>
       <button onClick={()=>setAge(age+1)}>Change Age</button>
+    
+    <hr />
+    <br />
+    <h1>Firends</h1>
+    {
+    firends.map((firend,i)=>(
+      <div key={i}>{firend}</div>
+    ))
+    }
+    
+ <button onClick={()=>setFirends(["X",...firends])}>Add Firends</button>
+   
     </div>
+    
   );
 }
 
